15e4194fb5f7c28e6e667e267ef729eca36ed1fd
yaoxuchen <yaoxuchen@df716528-d01d-7312-5286-586b6f8c7733>
2010-05-17 16:07:20 +0000

    Release for v1.1

---
+++ tags/PupSniffer-1.1/src/com/wcohen/ss/Levenstein.java
@@ -0,0 +1,33 @@
+package com.wcohen.ss;
+
+
+/**
+ * Levenstein string distance. Levenstein distance is basically
+ * NeedlemanWunsch with unit costs for all operations.
+ */
+
+public class Levenstein extends NeedlemanWunsch
+{
+	public Levenstein() {
+		super(CharMatchScore.DIST_01, 1.0 );
+	}
+	public String toString() { return "[Levenstein]"; }
+
+	static public void main(String[] argv) {
+		String s, t;
+		s = "http://www.cas.gov.hk/sctext/about/about_service.html";
+		t = "http://www.cas.gov.hk/ctext/about/about_service.html";
+//		s = "http://www.cas.gov.hk/ctext/notice/notice.html";
+//		t = "http://www.cas.gov.hk/etext/notice.html";
+		s = "http://www.cas.gov.hk/en/about/about_service.html";
+		t = "http://www.cas.gov.hk/ad/tc/about/about_service.html";
+		s = "http://www.wsd.gov.hk/tc/water_resources/raw_water_sources/water_sources_in_hong_kong/water_from_dongjiang_at_guangdong/index.html";
+		t = "http://www.wsd.gov.hk/tc/water_resources/raw_water_sources/water_sources_in_hong_kong/water_from_dongjiang_at_guangdong/shenzhen_water_supply_schematic_diagram/index.html";
+		Levenstein l = new Levenstein();
+		l.explainScore(s, t);
+
+		System.out.println("Diff Pair: "+l.getDiffPair());
+
+
+	}
+}
